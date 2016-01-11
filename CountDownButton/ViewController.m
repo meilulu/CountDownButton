b58cf48bcc5be6b4c1d5d8a5e5b5211678bb7fc6
威风堂堂 <84005870@qq.com>
2016-01-11 15:57:01 +0800

    Changes

--- CountDownButton/ViewController.m
+++ CountDownButton/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "UIButton+CountDown.h"
 @interface ViewController ()
 
 @end
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UIButton *countDownButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 50)];
+    countDownButton.backgroundColor = [UIColor colorWithRed:0/255.0 green:204/255.0 blue:255/255.0 alpha:1];
+    [countDownButton setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    countDownButton.center = CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2);
+    [countDownButton setTitle:@"倒计时" forState:UIControlStateNormal];
+    countDownButton.titleLabel.adjustsFontSizeToFitWidth = YES;
+    [countDownButton addTarget:self action:@selector(doCountDownAction:) forControlEvents:UIControlEventTouchUpInside];
+    countDownButton.layer.masksToBounds = YES;
+    countDownButton.layer.cornerRadius = 8.0f;
+    
+    [self.view addSubview:countDownButton];
+
+}
+
+- (void)doCountDownAction:(UIButton *)sender{
+    [sender startWithTime:10 title:@"开始倒计时" countDownTitle:@"秒" mainColor:[UIColor colorWithRed:0/255.0 green:204/255.0 blue:255/255.0 alpha:1] countColor:[UIColor grayColor]];
 }
 
 - (void)didReceiveMemoryWarning {
